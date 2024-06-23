2e29fdf8c9b541658b0e6ed9cf1c914d45cce16b
Daniel Kukula <daniel.kuku@gmail.com>
2024-06-23 10:00:56 +0100

    rustler_precompiled

--- lib/mdka_ex.ex
+++ lib/mdka_ex.ex
@@ -8,9 +8,21 @@ defmodule MdkaEx do
 end
 
 defmodule MdkaEx.Native do
-  use Rustler,
+  version = Mix.Project.config()[:version]
+
+  unsupported = [
+    "aarch64-unknown-linux-musl",
+    "x86_64-unknown-linux-musl",
+    "x86_64-unknown-freebsd"
+  ]
+
+  use RustlerPrecompiled,
     otp_app: :mdka_ex,
-    crate: :mdka_native
+    crate: :mdka_native,
+    version: version,
+    base_url: "https://github.com/dkuku/mdka_ex/releases/download/v#{version}",
+    force_build: System.get_env("MDKAEX_TEST") in ["1", "true"],
+    targets: Enum.uniq(RustlerPrecompiled.Config.default_targets() -- unsupported)
 
   def from_html(_arg1), do: :erlang.nif_error(:nif_not_loaded)
 end
--- mix.exs
+++ mix.exs
@@ -1,29 +1,55 @@
 defmodule MdkaEx.MixProject do
   use Mix.Project
 
-  def project do
+  @version "0.1.0"
+  def(project) do
     [
       app: :mdka_ex,
-      version: "0.1.0",
-      elixir: "~> 1.17",
+      version: @version,
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
+    ]
+  end
+
+  defp description() do
+    "mdka is a html to markdown converter using mdka-rs crate"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: [
+        "lib",
+        "mix.exs",
+        "native/mdka_native/.cargo",
+        "native/mdka_native/src",
+        "native/mdka_native/Cargo*",
+        "checksum-*.exs",
+        "README*"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "github" => "https://github.com/dkuku/mdka_ex"
+      }
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.33.0", runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.33.0", runtime: false, optional: true},
+      {:rustler_precompiled, "~> 0.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.33", only: :dev, runtime: false}
     ]
   end
 end
